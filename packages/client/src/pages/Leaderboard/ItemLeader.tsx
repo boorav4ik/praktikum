263216e309297f49a8ba69ce250856aeab96d6a6
tayru <denis.brudanin@mail.ru>
2023-03-11 11:25:25 +0300

    убрал стрелочную функцию

--- packages/client/src/pages/Leaderboard/ItemLeader.tsx
+++ packages/client/src/pages/Leaderboard/ItemLeader.tsx
@@ -1,61 +1,56 @@
-import { FC } from 'react'
 import { Box } from '@mui/material'
 import { AvatarBox } from '../../components/AvatarBox'
 import Typography from '@mui/material/Typography'
 
-interface ItemLeader {
+interface ItemLeaderProps {
   name: string
   score: string
   rating: number
 }
 
-export const ItemLeader: FC<ItemLeader> = ({
-  name,
-  score,
-  rating,
-}) => {
-  return (
-    <Box
-      sx={{
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'space-around',
-      }}>
-      <Box
-        sx={{
-          width: 20,
-          mr: '10px',
-        }}>
-        <Typography
-          component="span"
-          sx={{ fontWeight: 700, fontSize: 32, display: 'contents' }}
-          color="#C1EEE1">
-          {rating}
-        </Typography>
-      </Box>
+export function ItemLeader(props: ItemLeaderProps) {
+    return (
+        <Box
+            sx={{
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'space-around',
+            }}>
+            <Box
+                sx={{
+                    width: 20,
+                    mr: '10px',
+                }}>
+                <Typography
+                    component="span"
+                    sx={{ fontWeight: 700, fontSize: 32, display: 'contents' }}
+                    color="#C1EEE1">
+                    {props.rating}
+                </Typography>
+            </Box>
 
-      <AvatarBox />
-      <Box
-        sx={{
-          width: 100,
-          display: 'flex',
-          flexDirection: 'column',
-          ml: '25px',
-        }}>
-        <Typography
-          component="div"
-          sx={{ fontWeight: 700, fontSize: 20, display: 'block' }}
-          color="green.64">
-          {name}
-        </Typography>
+            <AvatarBox />
+            <Box
+                sx={{
+                    width: 100,
+                    display: 'flex',
+                    flexDirection: 'column',
+                    ml: '25px',
+                }}>
+                <Typography
+                    component="div"
+                    sx={{ fontWeight: 700, fontSize: 20, display: 'block' }}
+                    color="green.64">
+                    {props.name}
+                </Typography>
 
-        <Typography
-          component="div"
-          sx={{ fontWeight: 700, fontSize: 24, display: 'block' }}
-          color="white">
-          {score}
-        </Typography>
-      </Box>
-    </Box>
-  )
+                <Typography
+                    component="div"
+                    sx={{ fontWeight: 700, fontSize: 24, display: 'block' }}
+                    color="white">
+                    {props.score}
+                </Typography>
+            </Box>
+        </Box>
+    )
 }
