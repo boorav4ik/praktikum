20c1b18acc82268a6ef0bc84c7f2aab5a117abda
tayru <denis.brudanin@mail.ru>
2023-03-21 00:11:23 +0300

    промежуточный коммит

--- packages/client/src/layouts/Main/loginButtons/index.tsx
+++ packages/client/src/layouts/Main/loginButtons/index.tsx
@@ -1,19 +1,27 @@
 import { useAuth } from '../../../hooks/useAuth'
-import {AuthButton} from "./AuthButton";
-import {RegButton} from "./RegButton";
+import {AuthButton} from "./AuthButton"
+import {RegButton} from "./RegButton"
+import {Box} from "@mui/material"
+import {LinkButton} from "../../../components/LinkButton"
+import {Routes} from "../../../utils/routes"
+
 
 export function LoginButtons() {
   const [{ user }] = useAuth()
 
   return (
-      <div style={{
-          width: "fit-content",
+      <Box sx={{
           display: "flex",
           gap: "10px",
-          marginLeft: "30"
+          marginLeft: "30",
+          width: "290px",
+          justifyContent: "flex-end"
       }}>
-          <AuthButton/>
-          {!user && <RegButton/>}
-      </div>
+          <LinkButton sx={{ width: "fit-content"}}  to={`/${Routes.Login}`}>
+              {user ? 'Выйти' : 'Войти'}
+          </LinkButton>
+
+          {!user && <LinkButton sx={{ width: "fit-content"}} to={`/${Routes.Login}`}>Регистрация</LinkButton>}
+      </Box>
   )
 }
