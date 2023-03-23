abe3512df3297bc075e539157671bd3bd48b05ee
Replicatus <exotic2rus@gmail.com>
2023-03-23 17:01:24 +0300

    in progress

--- packages/client/src/components/forum/ForumRow/index.test.tsx
+++ packages/client/src/components/forum/ForumRow/index.test.tsx
@@ -1,15 +1,30 @@
-import { render, screen } from '@testing-library/react'
+import { render, screen, fireEvent } from '@testing-library/react'
 import {ForumRow} from './index'
+import {ChatAnswerIcon} from '../icons/ChatAnswerIcon'
 
 const text = 'test'
 const btnText = 'testBtn'
-const onClick = () => { console.log('test') }
+const onClick = jest.fn()
 describe('ForumRow component', () => {
   it('renders', () => {
     render(<ForumRow text={text} btnText={btnText} onClick={onClick}/>)
 
     expect(screen.getByText(text)).toBeInTheDocument()
     expect(screen.getByText(btnText)).toBeInTheDocument()
+  })
+
+  it('calls onClick', () => {
+    const { getByRole } = render(<ForumRow text={text} btnText={btnText} onClick={onClick}/>)
+    const btn = getByRole('button')
+
+    fireEvent.click(btn)
+    expect(onClick).toHaveBeenCalledTimes(1)
+  })
+
+  it('renders ChatAnswerIcon', () => {
+    render(<ForumRow text={text} btnText={btnText} onClick={onClick} icon={ChatAnswerIcon}/>)
+
+    expect(screen.getByRole('path')).toBeInTheDocument()
   })
 })
 export {}
