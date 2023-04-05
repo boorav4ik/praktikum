37345d1a29f18c24c40daba1edc882d56cd03bf7
Replicatus <exotic2rus@gmail.com>
2023-04-05 10:27:15 +0300

    in progress

--- packages/client/src/components/forum/HeaderForPage/index.test.tsx
+++ packages/client/src/components/forum/HeaderForPage/index.test.tsx
@@ -1,14 +1,11 @@
 import { render } from '@testing-library/react'
-import { BrowserRouter } from 'react-router-dom'
 import { HeaderForPage } from './index'
 
 describe('HeaderForPage component', () => {
   it('renders the text passed in the `text` prop', () => {
     const text = 'Hello, world!'
     const { getByText } = render(
-      <BrowserRouter>
         <HeaderForPage text={text} />
-      </BrowserRouter>
     )
     const headerText = getByText(text)
     expect(headerText).toBeInTheDocument()
@@ -17,9 +14,7 @@ describe('HeaderForPage component', () => {
   it('renders a back link when `backPath` is passed', () => {
     const backPath = '/previous-page'
     const { getByText } = render(
-      <BrowserRouter>
         <HeaderForPage text="Page Title" backPath={backPath} />
-      </BrowserRouter>
     )
     const backLink = getByText('Назад')
     expect(backLink).toBeInTheDocument()
@@ -28,9 +23,7 @@ describe('HeaderForPage component', () => {
 
   it('does not render a back link when `backPath` is not passed', () => {
     const { queryByText } = render(
-      <BrowserRouter>
         <HeaderForPage text="Page Title" />
-      </BrowserRouter>
     )
     const backLink = queryByText('Назад')
     expect(backLink).toBeInTheDocument()
