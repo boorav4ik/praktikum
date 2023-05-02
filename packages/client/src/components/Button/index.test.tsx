3ad9d626275527c3ee8ae5d8b0d9f75f72926214
Roman Rogozhin <ra.rogozhin@ined.ru>
2023-05-02 10:20:34 +0300

    fix formatting and fix dependencies

--- packages/client/src/components/Button/index.test.tsx
+++ packages/client/src/components/Button/index.test.tsx
@@ -1,23 +1,23 @@
-import {Button} from './index'
+import { Button } from './index'
 import { render } from '@testing-library/react'
 
 describe('Button component', () => {
   it('render ', () => {
-    const {getByRole} = render(<Button/>)
+    const { getByRole } = render(<Button />)
     const btn = getByRole('button')
 
     expect(btn).toBeInTheDocument()
   })
 
   it('applies props to component', () => {
-    const {getByRole} = render(<Button disabled={true}/>)
+    const { getByRole } = render(<Button disabled={true} />)
     const btn = getByRole('button')
 
     expect(btn).toHaveAttribute('disabled')
   })
 
   it('should render a button with the given variant', () => {
-    const { getByRole } = render(<Button variant="outlined" />)
+    const { getByRole } = render(<Button variant='outlined' />)
     expect(getByRole('button')).toHaveClass('MuiButton-outlined')
   })
 
