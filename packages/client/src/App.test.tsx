ab4f609aa230bee2a077bdc239af9cd381b4b93d
Max Trufanov <maxonline@mail.ru>
2023-05-09 13:12:03 +0300

    fix app test

--- packages/client/src/App.test.tsx
+++ packages/client/src/App.test.tsx
@@ -2,6 +2,7 @@ import { Provider } from 'react-redux'
 import App from './App'
 import { render, screen } from '@testing-library/react'
 import { store } from 'store'
+import { BrowserRouter } from 'react-router-dom'
 
 // @ts-ignore
 global.fetch = jest.fn(() =>
@@ -10,9 +11,11 @@ global.fetch = jest.fn(() =>
 
 test('Example test', async () => {
   render(
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
   )
   expect(screen.getByTestId('App')).toBeDefined()
 })
