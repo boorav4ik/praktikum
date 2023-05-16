8c5737b75f0361855aad1b22bc09267ac19d42d8
tayru <denis.brudanin@mail.ru>
2023-05-16 21:13:48 +0300

    правки по ревью Максима

--- packages/client/src/components/ErrorBoundary/index.tsx
+++ packages/client/src/components/ErrorBoundary/index.tsx
@@ -1,22 +1,12 @@
-import React, { Component } from 'react'
+import React, { Component, PropsWithChildren } from 'react'
 import { ErrorPage } from '../../pages/Error'
-
-interface ErrorBoundaryProps {
-  children: any
-}
 
 interface ErrorBoundaryState {
   hasError: boolean
 }
 
-class ErrorBoundary extends Component<
-  React.PropsWithChildren<ErrorBoundaryProps>,
-  ErrorBoundaryState
-> {
-  constructor(props: ErrorBoundaryProps) {
-    super(props)
-    this.state = { hasError: false }
-  }
+class ErrorBoundary extends Component<PropsWithChildren, ErrorBoundaryState> {
+  state = { hasError: false }
 
   static getDerivedStateFromError() {
     return { hasError: true }
